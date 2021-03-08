1af6d1862f0c67a3acb8cf1f3dfb6991f2acf33a
okadomeLab <okadomelab@okadomeLabnoMacBook-Pro.local>
2021-03-08 17:18:14 +0900

    -2.2

--- hello/views.py
+++ hello/views.py
@@ -5,8 +5,21 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
+    params = {
+        'title': 'Hello/Index',
+        'msg': 'This is sample page',
+        'goto': 'next',
+    }
     # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    return render(request, "hello/index.html", params)
+
+def next(request):
+    params = {
+        'title': 'Hello/Next',
+        'msg': 'This is another page',
+        'goto': 'index',
+    }
+    return render(request, 'hello/index.html', params)
 
 
 def db(request):
